59eee982e16bd81bfd8822eb7464e55cae319105
Nour Balaha <nourbalaha0@gmail.com>
2020-02-27 23:31:14 +0900

    wip firebase functions

---
+++ functions/index.js
@@ -0,0 +1,36 @@
+const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+const express = require("express");
+const app = express(); 
+
+app.get("/screams", (req, res) => {
+    admin.firestore().collection("screams").get()
+    .then(data => {
+        const screams = [];
+        data.forEach(doc => {
+            screams.push(doc.data());
+        })
+        return res.json(screams);
+    })
+    .catch(err => console.error(err));
+})
+
+app.post("/screams", (req, res) => {
+    const newScream = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    }
+
+    admin.firestore().collection("screams").add(newScream).then(doc => {
+        res.json({ message: `document ${doc.id} created successfuly`})
+    }).catch(err => {
+        res.status(500).json({ error: "something went wrong"});
+        console.log(err);
+    })
+});
+
+exports.api = functions.https.onRequest(app);
