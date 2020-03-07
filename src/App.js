d9413998745f03963d3220de9c3895009a79754c
Nour Balaha <nourbalaha0@gmail.com>
2020-03-07 15:58:29 +0900

    setting up main page and importing user input component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import UserInput from './components/UserInput/UserInput.component';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome To Social Eyes</h1>
-      </header>
+    <div className="app">
+      <main className="main">
+        <UserInput />
+        <div className="post">
+        </div>
+      </main>
+      <nav className="nav"></nav>
     </div>
   );
 }
