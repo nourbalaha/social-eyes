70f1329d997e84aae5078aaad29dc46e26ffb8c5
Nour Balaha <nourbalaha0@gmail.com>
2020-03-07 15:55:20 +0900

    created and working on user input component

---
+++ src/components/UserInput/UserInput.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './UserInput.style.scss';
+// import { ReactComponent as Logo } from '../../assets/user.svg'
+import avatar from '../../assets/avatar.png'
+
+function UserInput() {
+    return (
+        <div className="user-input">
+          <img className="user-input-image" src={avatar} alt="avatar" />
+          <input className="user-input-text" type="text" placeholder="What Are You Thinking?"/>
+          <button className="user-input-button">Submit</button>
+        </div>
+    )
+}
+
+export default UserInput;
