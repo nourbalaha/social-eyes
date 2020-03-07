bc87187df7e206be21e52d0bda804949ed4fd230
Nour Balaha <nourbalaha0@gmail.com>
2020-03-07 18:00:36 +0900

    wip post component and styles

--- src/components/Post/Post.component.jsx
+++ src/components/Post/Post.component.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import './Post.style.scss'
+import './Post.style.scss';
+
+import Logo from '../../assets/avatar.png';
 
 function Post() {
     return (
-        <div>
-            
+        <div className="post">
+            <div className="post-title">
+                <div className="post-user-container">
+                    <img className="post-title-icon" src={Logo} alt="post-title-icon" />
+                    <span className="post-title-username">Nour Balaha</span>
+                </div>
+                <span className="post-title-timestamp">14h ago</span>
+            </div>
+            <div className="post-body">
+                <p>Hello there, this is me Nour!</p>
+            </div>
         </div>
     )
 }
