abf965af22f2a3dc81dac289349b2836ee045991
Nour Balaha <nourbalaha0@gmail.com>
2020-03-07 18:01:27 +0900

    added font awesome icon and fixed styles

--- src/components/UserInput/UserInput.component.jsx
+++ src/components/UserInput/UserInput.component.jsx
@@ -7,7 +7,7 @@ function UserInput() {
         <div className="user-input">
           <img className="user-input-image" src={avatar} alt="avatar" />
           <input className="user-input-text" type="text" placeholder="What Are You Thinking?"/>
-          <button className="user-input-button">Submit</button>
+          <button className="user-input-button"><i className="fa fa-paper-plane"></i> Submit</button>
         </div>
     )
 }
