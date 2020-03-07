576877a89887eae408c3f1cc41cf57a29e5de884
Nour Balaha <nourbalaha0@gmail.com>
2020-03-07 17:59:57 +0900

    converted App.css to App.scss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 import UserInput from './components/UserInput/UserInput.component';
+import Post from './components/Post/Post.component';
 
 function App() {
   return (
     <div className="app">
       <main className="main">
         <UserInput />
-        <div className="post">
+        <div className="posts">
+          <Post />
+          <Post />
+          <Post />
         </div>
       </main>
       <nav className="nav"></nav>
