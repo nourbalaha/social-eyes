f2b89b1fd6839b64c21f60ef88f86f2d15db5d8f
Nour Balaha <nourbalaha0@gmail.com>
2020-03-09 02:36:53 +0900

    added icons styles and owl logo to navbar

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Navbar.style.scss';
 
+import Owl from '../../assets/owl.png'
+
 function Navbar() {
     return (
         <nav className="navbar">
-            <span><i className="nav-item fa fa-user-circle"></i></span>
-            <span><i class="nav-item fa fa-home"></i></span>
-            <span><i className="nav-item fa fa-cog"></i></span>
-            <span><i className="nav-item fa fa-sign-out"></i></span>
+            <div className="navbar-icons">
+                <span><i className="nav-item fa fa-user-circle"></i></span>
+                <span><i className="nav-item fa fa-home"></i></span>
+                <span><i className="nav-item fa fa-plus-circle"></i></span>
+                <span><i className="nav-item fa fa-cog"></i></span>
+                <span><i className="nav-item fa fa-sign-out"></i></span>
+            </div>
+            <img className="nav-owl" src={Owl} alt="owl" />
         </nav>
     )
 }
