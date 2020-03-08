9d7eecd139391cc88927d5f873e37e88e202f83d
Nour Balaha <nourbalaha0@gmail.com>
2020-03-09 00:59:48 +0900

    created posts component

---
+++ src/components/Posts/Posts.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Post from '../Post/Post.component'
+
+function Posts() {
+    return (
+        <div className="posts">
+          <Post />
+          <Post />
+          <Post />
+        </div>
+    )
+}
+
+export default Posts
