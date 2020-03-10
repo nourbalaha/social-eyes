51653763fdf37e019d82f10998611ff2481d6fed
Nour Balaha <nourbalaha0@gmail.com>
2020-03-10 23:51:22 +0900

    wip mock data

---
+++ src/mock_data.js
@@ -0,0 +1,23 @@
+export const mock_data = {
+    users:[
+        {
+            id:"",
+            username:"",
+            email:"",
+            description:"",
+            created_at:"",
+            friends:[],
+            likes:[],
+            posts:[
+                {
+                    post_id:"",
+                    text:"",
+                    likes:[],
+                    created_at:""
+                }
+            ],
+        }
+
+    ],
+
+}
