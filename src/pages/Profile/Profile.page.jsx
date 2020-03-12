343853c66f3147c18dc71cbc855e020a691a7ecd
Nour Balaha <nourbalaha0@gmail.com>
2020-03-13 02:31:32 +0900

    wip profile page left

--- src/pages/Profile/Profile.page.jsx
+++ src/pages/Profile/Profile.page.jsx
@@ -4,6 +4,8 @@ import "./Profile.style.scss"
 
 import { mock_data } from '../../mock_data'
 
+import Avatar from "../../assets/avatar.png"
+
 import UserInput from "../../components/UserInput/UserInput.component"
 import Posts from "../../components/Posts/Posts.component"
 
@@ -12,12 +14,15 @@ function Profile() {
     return (
         <div className="profile-page">
             <div className="profile-page-left">
-                <span>{mock_data["users"][id].userRef}</span>
-                <span>{mock_data["users"][id].email}</span>
-                <span>{mock_data["users"][id].description}</span>
-                <span>Edit description <i className="fa fa-pencil"></i></span>
-                <span>{`Likes: ${mock_data["users"][id].likes.length}`}</span>
-                <span>{`Posts: ${Object.keys(mock_data["users"][id].posts).length}`}</span>
+            <div className="profile-page-left-container">
+                <img className="profile-page-left-image" src={Avatar} alt="profile-pic" />
+                <span className="profile-page-left-user-ref">{mock_data["users"][id].userRef}</span>
+                <span className="profile-page-left-description">{mock_data["users"][id].description}</span>
+                <div className="profile-page-left-posts-likes-container">
+                    <span className="profile-page-left-likes">{`Likes: ${mock_data["users"][id].likes.length}`}</span>
+                    <span className="profile-page-left-posts">{`Posts: ${Object.keys(mock_data["users"][id].posts).length}`}</span>
+                </div>
+            </div>
             </div>
             <div className="profile-page-right">
                 <UserInput />
