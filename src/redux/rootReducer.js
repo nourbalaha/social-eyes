461939fe1d8a0df6710fe6c8cb7ee9d087cf8ccc
Nour Balaha <nourbalaha0@gmail.com>
2020-03-14 15:07:36 +0900

    created root reducer for redux

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,17 @@
+import postsReducer from "./posts/posts.reducer"
+
+import { combineReducers } from "redux"
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({
+    posts: postsReducer,
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+export default persistedReducer;
