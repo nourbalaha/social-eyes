7ae2c01c06f6f5218b476642bb42bf751a4cac49
Nour Balaha <nourbalaha0@gmail.com>
2020-03-14 17:38:07 +0900

    added add post action creater

--- src/redux/users/users.reducer.js
+++ src/redux/users/users.reducer.js
@@ -7,8 +7,24 @@ const initial_state = {
 // Reducer
 function usersReducer(state = initial_state, action) {
     switch (action.type) {  
+        case "ADD_POST":
+            const posts = state["users"]["users"][action.payload.userref]["posts"]
+            const length = String(Object.keys(posts).length)
+            console.log(Object.keys(posts)[length - 1])
+            const postId = String(Number(Object.keys(posts)[length - 1]) + 10)
+            const newPost = {
+                postId,
+                author: action.payload.userref,
+                message: action.payload.message,
+                likes: [],
+                createdAt: Date.now(),
+            }
+            const newState = {...state};
+            newState["users"]["users"][action.payload.userref]["posts"][postId] = newPost;
+            return newState;
+
         default:
-        return state;
+            return state;
     }
 }
   
