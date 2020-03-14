5d79b10881dc52ac9a24320a7b397db189c9bd25
Nour Balaha <nourbalaha0@gmail.com>
2020-03-14 15:04:51 +0900

    getting data from redux instead of mock_data file

--- src/components/Posts/Posts.component.jsx
+++ src/components/Posts/Posts.component.jsx
@@ -1,24 +1,33 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import Post from '../Post/Post.component'
 
 function Posts({ posts }) {
-  const keys = Object.keys(posts);
-  
-    return (
-        <div className="posts">
-          {
-            keys.map(
-              post=><Post 
-                id={posts[post]["postId"]} 
-                author={posts[post]["author"]} 
-                message={posts[post]["message"]} 
-                likes={posts[post]["likes"]} 
-                createdAt={posts[post]["createdAt"]} />
-              )
-          }
-        </div>
-    )
+  const id ="1"
+  const newPosts = posts["posts"]["users"][id]["posts"]
+  const keys = Object.keys(newPosts);
+
+  return (
+      <div className="posts">
+        {
+          keys.map(
+            post=><Post
+              key={newPosts[post]["postId"]} 
+              id={newPosts[post]["postId"]} 
+              author={newPosts[post]["author"]} 
+              message={newPosts[post]["message"]} 
+              likes={newPosts[post]["likes"]} 
+              createdAt={newPosts[post]["createdAt"]} />
+            )
+        }
+      </div>
+  )
 }
 
-export default Posts
+function mapState (state) {
+  return { posts: state.posts }
+}
+
+// Connect them:
+export default connect(mapState)(Posts)
