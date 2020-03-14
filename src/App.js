4f92bd29ab94c8a1ae442f0f89275a085477ece6
Nour Balaha <nourbalaha0@gmail.com>
2020-03-14 17:46:27 +0900

    adde home page to imports and removed some routes

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar.component';
 
 import Profile from './pages/Profile/Profile.page'
+import Home from './pages/Home/Home.page'
 
 function App() {
   return (
     <div className="app">
       <main className="main">
         <Switch>
-          <Route exact path="/" component={Profile} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/profile/:id" component={Profile} />
+          <Route exact path="/" component={Home} />
+          <Route path="/profile/:userref" component={Profile} />
         </Switch>
       </main>
       <Navbar />
