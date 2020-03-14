0a1045613d99ae42b418f2d1e3c7beb5d46d3cfe
Nour Balaha <nourbalaha0@gmail.com>
2020-03-15 00:04:47 +0900

    added left and right sections and styles to home component

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import './Home.style.scss'
+
 function Home() {
     return (
-        <div>
-            Home
+        <div className="home-page">
+            <div className="home-page-left">
+                <div className="home-page-left-container">
+                    Left Section
+                </div>
+            </div>
+
+            <div className="home-page-right">
+                Right Section
+            </div>
         </div>
     )
 }
