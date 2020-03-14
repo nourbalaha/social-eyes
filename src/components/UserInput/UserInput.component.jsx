003a80a75b89d7799a487aef6d07bfa10be2e885
Nour Balaha <nourbalaha0@gmail.com>
2020-03-14 17:39:35 +0900

    added styles and functionality to user input

--- src/components/UserInput/UserInput.component.jsx
+++ src/components/UserInput/UserInput.component.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+
 import './UserInput.style.scss';
 import avatar from '../../assets/avatar.png'
 
-function UserInput() {
+function UserInput({ onSubmit, match }) {
+  const [input, setInput] = useState("");
+
+  const handleChange = event => {
+    setInput(event.target.value)
+  }
+
+  const handleSubmit = () => {
+    onSubmit(match.params.userref, input);
+    setInput("")
+  }
     return (
         <div className="user-input">
           <img className="user-input-image" src={avatar} alt="avatar" />
-          <input className="user-input-text" type="text" placeholder="What Are You Thinking?"/>
-          <button className="user-input-button"><i className="fa fa-paper-plane"></i> Submit</button>
+          <input className="user-input-text" type="text" placeholder="What Are You Thinking?" value={input} onChange={handleChange} />
+          <button className="user-input-button" onClick={handleSubmit}><i className="fa fa-paper-plane"></i> Submit</button>
         </div>
     )
 }
 
-export default UserInput;
+const mapDispatch = dispatch => {
+  return {
+    onSubmit (userref, message) {
+      dispatch({ type: 'ADD_POST', payload: {userref, message} })
+    }
+  }
+}
+
+export default compose(withRouter, connect(null, mapDispatch))(UserInput);
