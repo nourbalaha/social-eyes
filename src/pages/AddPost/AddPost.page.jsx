a18f2546ef60cdcdf31bdd38618496c38471b3d3
Nour Balaha <nourbalaha0@gmail.com>
2020-03-16 01:00:25 +0900

    created add post page and initial styles

---
+++ src/pages/AddPost/AddPost.page.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './AddPost.style.scss'
+
+function AddPost() {
+    return (
+        <div className="add-post-page">
+            <div className="add-post-page-left">
+                <div className="add-post-page-left-container">
+                    Left Section
+                </div>
+            </div>
+
+            <div className="add-post-page-right">
+                Right Section
+            </div>
+        </div>
+    )
+}
+
+export default AddPost
