e2b7cc451766c4413c5e375eb23095be031bdc14
Nour Balaha <nourbalaha0@gmail.com>
2020-03-16 01:00:56 +0900

    created settings page and initial styles

---
+++ src/pages/Settings/Settings.page.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './Settings.style.scss'
+
+function Settings() {
+    return (
+        <div className="settings-page">
+            <div className="settings-page-left">
+                <div className="settings-page-left-container">
+                    Left Section
+                </div>
+            </div>
+
+            <div className="settings-page-right">
+                Right Section
+            </div>
+        </div>
+    )
+}
+
+export default Settings;
