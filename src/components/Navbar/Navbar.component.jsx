53364021a142c3590ce884ff67c19991148f25a2
Nour Balaha <nourbalaha0@gmail.com>
2020-03-20 23:04:53 +0900

    added navigation to navbar items

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -11,8 +11,8 @@ function Navbar({ history }) {
             <div className="navbar-icons">
                 <span onClick={()=>history.push(`/profile/${userRef}`)}><i className="nav-item fa fa-user-circle"></i></span>
                 <span onClick={()=>history.push(`/`)}><i className="nav-item fa fa-home"></i></span>
-                <span><i className="nav-item fa fa-plus-circle"></i></span>
-                <span><i className="nav-item fa fa-cog"></i></span>
+                <span onClick={()=>history.push(`/addpost/`)}><i className="nav-item fa fa-plus-circle"></i></span>
+                <span onClick={()=>history.push(`/settings/`)}><i className="nav-item fa fa-cog"></i></span>
                 <span><i className="nav-item fa fa-sign-out"></i></span>
             </div>
             <img className="nav-owl" src={Owl} alt="owl" />
