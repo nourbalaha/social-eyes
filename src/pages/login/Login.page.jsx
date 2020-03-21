a2ed4cf07861a5306e3deacedb97448a62ebbd7d
Nour Balaha <nourbalaha0@gmail.com>
2020-03-21 16:53:21 +0900

    wip loginfunctions and styles2

--- src/pages/login/Login.page.jsx
+++ src/pages/login/Login.page.jsx
@@ -1,16 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Login.style.scss';
 
-function Login() {
-    return (
-        <div className="login">>
-           <input className="Login-email" type="email" placeholder="Email" value="" /> 
-           <input className="Login-password" type="password" placeholder="Password" value="" />
-           <button className="login-button">Login</button>
-           <p>Don't have an account. <span>Register</span> here</p>
-        </div>
-    )
+import Logo from '../../assets/owl.png';
+
+import { auth } from '../../firebase/firebase.config';
+
+function Login ({ history }) {
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = event => {
+    const { name, value } = event.target
+
+    setUser({
+      ...user,
+      [name]: value
+    })
+  }
+
+  const handleSignIn = async () => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      try {
+        await auth.signInWithEmailAndPassword(user.email, user.password)
+      } catch (error) {
+        console.log(error.message)
+      }
+      history.push('/home')
+    } else {
+      console.log('please enter required field')
+    }
+  }
+
+  return (
+    <div className='login'>
+      <div className='login-container'>
+        <img className='login-image' src={Logo} alt='logo' />
+        <span className='login-header-upper'>Welcome to</span>
+        <span className='login-header-lower'>Social Eyes</span>
+        <input
+          className='login-email'
+          name='email'
+          type='email'
+          placeholder='Email'
+          value={user.email}
+          onChange={handleChange}
+        />
+        <input
+          className='login-password'
+          name='password'
+          type='password'
+          placeholder='Password'
+          value={user.password}
+          onChange={handleChange}
+        />
+        <button className='login-button' onClick={handleSignIn}>
+          Login
+        </button>
+        <p className='login-text'>
+          Don't have an account.{' '}
+          <span className='login-register' onClick={()=>history.push("/register")}>Register</span> here.
+        </p>
+      </div>
+    </div>
+  )
 }
 
-export default Login;
+export default Login
