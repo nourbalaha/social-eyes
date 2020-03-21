71eeaecbf1bac46b8ca40a85d95aa58bbf77e880
Nour Balaha <nourbalaha0@gmail.com>
2020-03-21 16:28:43 +0900

    added functionality to sign out button

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom';
+
+import { auth } from '../../firebase/firebase.config';
 import './Navbar.style.scss';
 
-import Owl from '../../assets/owl.png'
+import Owl from '../../assets/owl.png';
 
 function Navbar({ history }) {
     const userRef = "nourbalaha"
+
+    const handleSignout = async () => {
+        try {
+          await auth.signOut();
+          console.log({ msg:"You Have Been Logged Out Succefully!", type: "success" })
+        } catch(error) {
+          console.log({ msg:error.message, type: "error" })
+        };
+      };
+
     return (
         <nav className="navbar">
             <div className="navbar-icons">
@@ -13,7 +25,7 @@ function Navbar({ history }) {
                 <span onClick={()=>history.push(`/`)}><i className="nav-item fa fa-home"></i></span>
                 <span onClick={()=>history.push(`/addpost/`)}><i className="nav-item fa fa-plus-circle"></i></span>
                 <span onClick={()=>history.push(`/settings/`)}><i className="nav-item fa fa-cog"></i></span>
-                <span><i className="nav-item fa fa-sign-out"></i></span>
+                <span onClick={handleSignout}><i className="nav-item fa fa-sign-out"></i></span>
             </div>
             <img className="nav-owl" src={Owl} alt="owl" />
         </nav>
