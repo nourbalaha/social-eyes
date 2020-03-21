20874447238697402ed69ab45c07e38968033ad6
Nour Balaha <nourbalaha0@gmail.com>
2020-03-21 19:51:45 +0900

    added UserProfile component as left section on setting  page

--- src/pages/Settings/Settings.page.jsx
+++ src/pages/Settings/Settings.page.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-import './Settings.style.scss'
+import './Settings.style.scss';
+
+import UserProfile from '../../components/UserProfile/UserProfile.component';
 
 function Settings() {
     return (
         <div className="settings-page">
             <div className="settings-page-left">
-                <div className="settings-page-left-container">
-                    Left Section
-                </div>
+                <UserProfile />
             </div>
 
             <div className="settings-page-right">
