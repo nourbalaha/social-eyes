d5c28262654f7deee04f317f2f62c9e2f228ab60
Nour Balaha <nourbalaha0@gmail.com>
2020-03-21 14:54:12 +0900

    created firebase config file

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,28 @@
+import firebase from "firebase/app"
+import "firebase/firestore"
+import "firebase/auth"
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB_xkFzhVpVr9QC2k33P5CccxFG19xfb18",
+    authDomain: "socia1eyes.firebaseapp.com",
+    databaseURL: "https://socia1eyes.firebaseio.com",
+    projectId: "socia1eyes",
+    storageBucket: "socia1eyes.appspot.com",
+    messagingSenderId: "973196236998",
+    appId: "1:973196236998:web:9d85d953310f3cfb631fc7",
+    measurementId: "G-BC9B2WSVJG"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: "select_account"});
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
