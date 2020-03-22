df26ecd8d0f9da6c96ed2debc252e6e40b8d2680
Nour Balaha <nourbalaha0@gmail.com>
2020-03-22 21:49:00 +0900

    used hooks and firestore

--- src/components/UserProfile/UserProfile.component.jsx
+++ src/components/UserProfile/UserProfile.component.jsx
@@ -1,30 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+
+import { firestore } from '../../firebase/firebase.config';
 
 import "./UserProfile.style.scss";
 
 import Avatar from "../../assets/avatar.png";
 
-function UserProfile({ match, users }) {
-    const userRef = match.params.userref;
+function UserProfile({ currentUser }) {
+    const [user, setUser] = useState({userRef:"", description:"", likes:[], posts:{}})
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            const profileRef = firestore.collection("users").doc(currentUser.displayName);
+            const profileSnap = await profileRef.get();
+            setUser(profileSnap.data())
+        }
+        fetchData()
+    },[currentUser.displayName])
+
     return (
         <div className="profile-page-left-container">
             <img className="profile-page-left-image" src={Avatar} alt="profile-pic" />
-            <span className="profile-page-left-user-ref">{users["users"]["users"][userRef].userRef}</span>
-            <span className="profile-page-left-description">{users["users"]["users"][userRef].description}</span>
+            <span className="profile-page-left-user-ref">{user.userRef}</span>
+            <span className="profile-page-left-description">{user.description}</span>
             <div className="profile-page-left-posts-likes-container">
-                <span className="profile-page-left-likes">{`Likes: ${users["users"]["users"][userRef].likes.length}`}</span>
-                <span className="profile-page-left-posts">{`Posts: ${Object.keys(users["users"]["users"][userRef].posts).length}`}</span>
+                <span className="profile-page-left-likes">{`Likes: ${user.likes.length}`}</span>
+                <span className="profile-page-left-posts">{`Posts: ${Object.keys(user.posts).length}`}</span>
             </div>
         </div>
     )
 }
 
 function mapState (state) {
-    return { users: state.users }
+    return { 
+        currentUser: state.auth.currentUser,
+    }
 }
   
-// Connect them:
 export default compose(withRouter, connect(mapState))(UserProfile)
