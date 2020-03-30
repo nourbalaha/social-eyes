33db995ecec64bf84a519d9fd457ca665fee818d
Nour Balaha <nourbalaha0@gmail.com>
2020-03-31 00:52:19 +0900

    wip like feed post action and reducer

--- src/redux/feed/feed.actions.js
+++ src/redux/feed/feed.actions.js
@@ -16,10 +16,36 @@ export const getFeed = () => {
             return data.map(result=>result.docs[0].data());
         })
 
+        let obj = {}
+        result.forEach(post=>obj[post.postId]=post)
 
         dispatch({
             type: "GET_FEED",
-            payload: result,
+            payload: obj,
+        })
+    }
+}
+
+export const likeFeedPost = (post) => {
+    return async (dispatch, getState) => {
+        const currentUserRef = getState().auth.currentUser.displayName; 
+        const ref = await firestore.collection("users").doc(post.userRef).collection("posts").doc(post.postId);
+        let result = await ref.get()
+        result = result.data();
+        const likes = result["likes"];
+
+        if(!likes.includes(currentUserRef)){
+            likes.push(currentUserRef);
+        } else {
+            let index = likes.indexOf(currentUserRef);
+            likes.splice(index, 1);
+        }
+
+        await ref.update(result)
+    
+        dispatch({
+            type:"LIKE_POST", 
+            payload: post
         })
     }
 }
--- src/redux/feed/feed.reducer.js
+++ src/redux/feed/feed.reducer.js
@@ -1,12 +1,24 @@
 // Initial State
-const initial_state = [];
+const initial_state = {};
   
 // Reducer
 function feedReducer(state = initial_state, action) {
     switch (action.type) {  
         case "GET_FEED":
-            const newState = action.payload;
-            return newState;
+            const newFeedState = action.payload;
+            return newFeedState;
+    
+            case "LIKE_FEED_POST":
+                const newStateAfterLike = {...state};
+                const postIdToLike = action.payload.postId;
+                const likes = newStateAfterLike[postIdToLike]["likes"];
+                if(!likes.includes(action.payload.current)){
+                    likes.push(action.payload.current);
+                } else {    
+                    let index = likes.indexOf(action.payload.current);
+                    likes.splice(index, 1);
+                }
+                return newStateAfterLike;
 
         default:
             return state;
