1164a09783dcd5d7be0f648cae306db57325b433
Nour Balaha <nourbalaha0@gmail.com>
2020-04-06 02:43:56 +0900

    wip settings section

--- src/components/SettingsSection/SettingsSection.component.jsx
+++ src/components/SettingsSection/SettingsSection.component.jsx
@@ -1,25 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
 import { getFeed } from '../../redux/feed/feed.actions'
 
-import SwitchUI from '../SwitchUI/SwitchUI.material'
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
 import './SettingsSection.style.scss'
 
-function SettingsSection() {
+function SettingsSection({ user }) {
+  const [checked, setChecked] = useState(user.openProfile);
+
+  const handleSwitch = (event) => {
+    setChecked(!checked);
+  };
+
   return (
       <div className="settings-section">
-        <SwitchUI />
+      
+        <TextField id="standard-basic" className="settings-section-userref" label="UserRef" />
+      
+        <TextField
+        id="outlined-multiline-static"
+        className="settings-section-description" 
+        label="Description"
+        multiline
+        rows="4"
+        defaultValue="Default Value"
+        variant="outlined"
+        />
+        
+        <FormControlLabel
+        className="settings-section-switch" 
+        control={<Switch checked={checked} onChange={handleSwitch} name="openProfile" />}
+        label="Open Profile"
+        />
+        
+          <Button
+          className="settings-section-apply-btn" 
+          variant="contained"
+          color="default"
+          startIcon={<CloudUploadIcon />}
+          >        
+          Apply Changes
+          </Button>
       </div>
   )
 }
 
 function mapState (state) {
   return { 
-    feed: state.feed,
+    user: state.user,
   }
 }
 
@@ -32,3 +68,4 @@ const mapDispatch = dispatch => {
 }
 
 export default compose(withRouter, connect(mapState,mapDispatch))(SettingsSection)
+
