e362f4cdbad6390178d788237aaa68babbd2d068
Nour Balaha <nourbalaha0@gmail.com>
2020-04-06 04:43:26 +0900

    finished update profile in the settings

--- src/components/SettingsSection/SettingsSection.component.jsx
+++ src/components/SettingsSection/SettingsSection.component.jsx
@@ -3,7 +3,7 @@ import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
-import { getFeed } from '../../redux/feed/feed.actions'
+import { setUserData } from '../../redux/user/user.actions'
 
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
@@ -13,42 +13,56 @@ import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
 import './SettingsSection.style.scss'
 
-function SettingsSection({ user }) {
-  const [checked, setChecked] = useState(user.openProfile);
+function SettingsSection({ user, onSetUser }) {
+  const [state, setState] = useState(user);
 
   const handleSwitch = (event) => {
-    setChecked(!checked);
+    setState({...state, openProfile: event.target.checked});
+  };
+
+  const handleChange = (event) => {
+    setState({...state, [event.target.name]: event.target.value});
+  };
+
+  const handleApply = () => {
+    onSetUser(state);
   };
 
   return (
-      <div className="settings-section">
-      
-        <TextField id="standard-basic" className="settings-section-userref" label="UserRef" />
-      
-        <TextField
-        id="outlined-multiline-static"
-        className="settings-section-description" 
-        label="Description"
-        multiline
-        rows="4"
-        defaultValue="Default Value"
-        variant="outlined"
-        />
-        
-        <FormControlLabel
-        className="settings-section-switch" 
-        control={<Switch checked={checked} onChange={handleSwitch} name="openProfile" />}
-        label="Open Profile"
-        />
-        
+    <div className="settings-section">
+      <form className="settings-section-form">
+       <fieldset  className="settings-section-fieldset">
+        <legend>PROFILE SETTINGS</legend>
+          <TextField className="settings-section-userref" label="UserRef" value={state.userRef} name="userRef" onChange={handleChange} />
+          <TextField className="settings-section-email" label="Email" value={state.email}  name="email" onChange={handleChange}  />
+          <TextField
+          className="settings-section-description" 
+          label="Description"
+          multiline
+          rows="4"
+          value={state.description} 
+          variant="outlined"
+          name="description"
+          onChange={handleChange} 
+          />
+
+          <FormControlLabel
+          className="settings-section-switch" 
+          control={<Switch checked={state.openProfile} onChange={handleSwitch} name="openProfile" />}
+          label="Open Profile"
+          />
+          
           <Button
           className="settings-section-apply-btn" 
           variant="contained"
           color="default"
           startIcon={<CloudUploadIcon />}
+          onClick={handleApply}
           >        
           Apply Changes
           </Button>
+        </fieldset>
+       </form>
       </div>
   )
 }
@@ -61,8 +75,8 @@ function mapState (state) {
 
 const mapDispatch = dispatch => {
   return {
-    onGetFeed() {
-      dispatch(getFeed());
+    onSetUser(user) {
+      dispatch(setUserData(user));
     }
   }
 }
