6fcb3283ea128090526e30980b2e25ed94da809d
Nour Balaha <nourbalaha0@gmail.com>
2020-05-04 14:47:57 +0900

    can not enter non open profile profiles

--- src/pages/Profile/Profile.page.jsx
+++ src/pages/Profile/Profile.page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import "./Profile.style.scss";
 
@@ -6,19 +7,35 @@ import UserInput from "../../components/UserInput/UserInput.component";
 import Posts from "../../components/Posts/Posts.component";
 import UserProfile from "../../components/UserProfile/UserProfile.component";
 
-function Profile() {
+function Profile({ match, user, users, currentUser }) {
     return (
         <div className="profile-page">
             <div className="profile-page-left">
                 <UserProfile />
             </div>
-            <div className="profile-page-right">
-                <UserInput />
-                <Posts />
-            </div>
+            {
+                (users[match.params.userref].openProfile || currentUser.displayName===match.params.userref)
+                ?
+                <div className="profile-page-right">
+                    <UserInput />
+                    <Posts />
+                </div>
+                :
+                <div className="profile-page-right">
+                    <p>Profile is not open to public</p>
+                </div>
+                }
         </div>
     )
 }
 
-// Connect them:
-export default Profile;
+
+function mapState (state) {
+    return { 
+      currentUser: state.auth.currentUser,
+      user: state.user,
+      users: state.users,
+    }
+  }
+  
+  export default connect(mapState)(Profile)
