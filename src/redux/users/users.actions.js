ac74aaca47c60d08942bb83af17245756bb4d551
Nour Balaha <nourbalaha0@gmail.com>
2020-06-28 14:31:01 +0900

    added async await to one of the lines

--- src/redux/users/users.actions.js
+++ src/redux/users/users.actions.js
@@ -4,7 +4,7 @@ export const getUsers = () => {
     return async (dispatch, getState) => {
         const userRef = firestore.collection("users");
         const userSnap = await userRef.get();
-        let result = userSnap.docs.map(doc=>doc.data());
+        let result = await userSnap.docs.map(doc=>doc.data());
         const users = {};
         result.forEach(user=>{
             users[user.userRef]=user;
